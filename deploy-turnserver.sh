63420df193fd0c3dc25b028577e8772e89cae660
Cracker <rofl256@googlemail.com>
2020-02-24 14:04:47 +0100

    enable use-auth-secret

--- deploy-turnserver.sh
+++ deploy-turnserver.sh
@@ -4,6 +4,7 @@ echo $3 #realm
 echo $4 #listening ips
 echo $5 #replay ip
 echo $6 #external ip
+echo $7 #authsecret
 
 echo "listening-port=443
 tls-listening-port=4433
@@ -12,6 +13,8 @@ relay-ip="$5"
 external-ip="$6"
 realm=$3
 server-name=$3
+use-auth-secret=true
+static-auth-secret="$7"
 lt-cred-mech
 userdb=/var/lib/turn/turndb
 # use real-valid certificate/privatekey files
